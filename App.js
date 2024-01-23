67f67327367ad9b9841e5ada6640580ce59dfe91
rafaDev <rrg1459@hotmail.com>
2024-01-23 11:38:11 -0400

    Add actual location to App.js

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+
 import MapView from 'react-native-maps';
 
+import * as Location from 'expo-location';
+
 export default function App() {
-  return (
+ const [position, setPosition] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+ });
+
+ useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setPosition({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    })();
+}, []);
+  
+return (
     <View style={styles.container}>
-      <MapView style={styles.map}></MapView>
+      <MapView 
+        style={styles.map}
+        initialRegion={position}
+        region={position}></MapView>
       <StatusBar style="auto" />
     </View>
   );
